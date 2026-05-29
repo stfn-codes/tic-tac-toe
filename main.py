fb2eb2fc556944660dbe0107e37b50cc06ba28b4
Stefano <borzstefano@gmail.com>
2026-05-29 20:33:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import pygame, sys
 
+screen = pygame.display.set_mode((300, 300))
+clock = pygame.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+    pygame.display.flip()
